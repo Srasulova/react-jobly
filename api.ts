f76ea3482ebf733ab74e6dd6ca12c0910994ed64
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-08-13 20:52:20 -0400

    fix the config bugs

--- api.ts
+++ api.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const BASE_URL: string =
-  process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+  import.meta.env.VITE_BASE_URL || "http://localhost:3001";
+
+//const BASE_URL: string = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 /** Interface for the API response */
 interface ApiResponse<T> {
